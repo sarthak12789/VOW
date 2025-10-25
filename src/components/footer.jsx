121ec7d81072d77efb51f86a9a997d5ee81a6e6f
yashvi <yashvisaini.ys@gmail.com>
2025-10-25 15:01:11 +0530

    footer layout fixed as per design

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { Link } from 'react-router-dom'; 
 
 const Footer = () => {
   return (
@@ -8,7 +9,7 @@ const Footer = () => {
       className="text-[#EFEFEF] bg-gradient-to-b from-[#1B0837] to-[#240A44] px-[60px] py-[60px]"
     >
       {/* Container */}
-      <div className="max-w-[1160px] mx-auto flex flex-col lg:flex-row justify-center items-start gap-[48px]">
+      <div className="max-w-[1160px] mx-auto flex flex-col lg:flex-row justify-center items-start gap-[164px]">
         {/* Brand / Intro */}
         <section className="w-full max-w-[375px] space-y-4">
           <div className="flex items-center gap-3 mb-[59px]">
@@ -19,7 +20,7 @@ const Footer = () => {
             />
             <div className="leading-none ml-[38px]">
               <h3 className="text-[32px] font-bold text-[#FEFEFE] pb-[3px]">VOW</h3>
-              <p className="text-[26px]font-normal text-[#FEFEFE] ">Virtual Organised World</p>
+              <p className="text-[24px] font-normal text-[#FEFEFE] ">Virtual Organised World</p>
             </div>
           </div>
 
@@ -27,13 +28,13 @@ const Footer = () => {
             Experience the most natural way to collaborate remotely.
           </p>
 
-          <div className="flex flex-wrap items-center ">
-            <a
-              href="#get-started"
+          <div className="flex flex-nowrap items-center ">
+            <Link
+              to="/signup"
               className="px-[36px] py-[10px] rounded-[8px] w-[183px] h-[39px] mr-[8px] text-center font-normal bg-[#5E9BFF] text-[#FEFEFE] border border-[#EFEFEF] "
             >
               Get Started
-            </a>
+            </Link>
             <a
               href="#watch-demo"
               className="px-[36px] py-[10px] rounded-[8px] w-[183px] h-[39px] font-normal border border-[#EFEFEF] text-[#EDEBFF]"
